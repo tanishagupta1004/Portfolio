5844ff713c564d0ba781430d83f19a54c9d5d4f5
Tanisha Gupta <tanishagupta382@gmail.com>
2023-06-01 14:59:51 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## tanishaaa Portfolio Website
---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header= document.getElementById('header')
+
+    if(this.scrollY >=50) header.classList.add('scroll-header')
+}
+window.addEventListener('scroll',scrollHeader)
+/*=============== SERVICES MODAL ===============*/
+const modalViews=document.querySelectorAll('.services__modal'),
+    modalBtns=document.querySelectorAll('.services__button'),
+    modalClose=document.querySelectorAll('.services__modal-close')
+
+
+let modal=function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb,i) => {
+    mb.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) =>{
+    mc.addEventListener('click', ()=>{
+        modalViews.forEach((mv)=>{
+            mv.classList.remove('active-modal')
+        })
+    })
+})
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* Link active work */ 
+const linkWork=document.querySelectorAll('.work__item')
+
+function activeWork()
+{
+    linkWork.forEach(l=>l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener('click', activeWork))
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== LIGHT DARK THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
